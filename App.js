a56ad8df3e63d2862cb8c5c4b9f554ff6b359f72
Anouar-91 <a.berrouane@hotmail.com>
2022-05-11 14:20:58 +0200

    first commit : style + Text + Button

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native'
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.wrapper}>  
+      <Text style={styles.text}>{count}</Text>
+      <View     style={styles.button}>
+        <Button
+          onPress={() => setCount(count + 1)}
+          title="+1"
+          color="white"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  wrapper : {
+    marginTop:50,
+    flexDirection:"column"
   },
-});
+  text: {
+    textAlign: "center",
+    fontSize:50
+
+  },
+  button: {
+    padding: 20,
+    color: "red",
+    backgroundColor:"purple"
+  }
+
+})
